bca5285304909c890067766c78c0e6bb7e1a5d7a
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-10 18:48:05 +0500

    Added class and basic set up

---
+++ my_class.rb
@@ -0,0 +1,11 @@
+require 'MyEnumerable'
+
+class  MyList
+  include MyEnumerable
+   def initialize(*list)
+    @list = list
+  end
+  def each(&block)
+    @list.each(&block)
+  end
+end
---
+++ my_module.rb
@@ -0,0 +1,3 @@
+module MyEnumerable
+  
+end
