049811d1a536adf2c6fe3f4d004a33048c79e8bb
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-10 16:56:49 +0300

    Add methods of enumerable

--- my_module.rb
+++ my_module.rb
@@ -1,3 +1,22 @@
 module MyEnumerable
+  def all?(&block)
+    each do |item|
+        return false unless yield(item)
+    end
+    true
+  end
+
+  def any?(&block)
+    each do |item|
+        return true if yield(item)
+    end
+    false
+  end
   
+  def filter(&block)
+    result = []
+    each do |item| result << item if yield(item)
+    end
+    result
+  end
 end
